2b6e0be1a561a8e7ef5829ad6c44d262209c5bc3
gustavo domingues <deathdiec@gmail.com>
2022-04-01 14:59:19 -0300

    v1.1

--- Assets/CHANGELOG.md
+++ Assets/CHANGELOG.md
@@ -1,2 +1,11 @@
+# v1.1
+- Added the TagQuery, a static script that can handle all the tags you added in the scene.
+```
+TagQuery.FindObject("tagName");
+
+It uses a dictionary to loop through the objects faster than the findobjectsoftype.
+```
+- Folder organization
+
 # V1.00
 - Release with all the base functions working
---
+++ Scripts/Tags/TagQuery.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public static class TagQuery
+{
+    private static Dictionary<string, GameObject> cachedObjects = new Dictionary<string, GameObject>();
+
+    public static GameObject FindObject(string name){
+        GameObject obj;
+        if(cachedObjects.TryGetValue(name, out obj)){
+            return obj;
+        }else{
+            var o = Object.FindObjectsOfType<Tag>();
+            foreach(var g in o){
+                cachedObjects.Add(g.tagName, g.gameObject);
+                if(g.tagName == name)return g.gameObject;
+            }
+        }
+        return null;
+    }
+    public static void DisposeCache(){
+        cachedObjects.Clear();
+    }
+}
